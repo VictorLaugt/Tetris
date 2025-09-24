e16e4a66417ca046678e97b308e957b9470289b1
LAUGT Victor <victorlaugtdev@gmail.com>
2025-09-24 23:12:23 +0200

    draw background once

--- tetris_back.py
+++ tetris_back.py
@@ -13,24 +13,24 @@ class Direction:
     right = np.array([1, 0])
     up =    np.array([0, -1])
     down =  np.array([0, 1])
-    
+
 class Rotation:
     direct = 1
     indirect = -1
-    
-    
+
+
 def random_color():
     return random.randrange(1, 7)
 
 
 class Block:
     """Implements Tetris blocks
-    
+
     Interface
     ----------------------------------------------------------------------------
     attributes:
         color: int
-    
+
     methods:
         get_squares
         inside_limits
@@ -41,12 +41,12 @@ class Block:
     """
     models: VectorGrid
     radius: int
-    
-    def __init__(self, x, y, color):        
+
+    def __init__(self, x, y, color):
         self.shapes = self.models.copy()
         self.shapes[:, :, 0] += x
         self.shapes[:, :, 1] += y
-        
+
         self.orientation = 0
         self.color = color
 
@@ -65,26 +65,26 @@ class Block:
                     row.append('.')
             result.append(''.join(row))
         return '\n'.join(result)
-    
+
     def inside_limits(self, width, height):
         sqrs = self.get_squares()
         return np.all((0, 0) <= sqrs) and np.all(sqrs < (width, height))
-    
+
     def colides(self, grid):
         return any(grid[x, y] for x, y in self.get_squares())
-    
+
     def shift(self, vector):
         self.shapes += vector
-    
+
     def rotate(self, rotation):
         self.orientation = (self.orientation + rotation) % len(self.models)
-    
+
     def lock(self, grid):
         for x, y in self.get_squares():
             grid[x, y] = self.color
 
 
-class BlockO(Block):    
+class BlockO(Block):
     models = np.array(( ((0,0), (1,0), (0,1), (1,1)), ), dtype=np.int64)
     y_start = -2
     x_min = 0
@@ -161,7 +161,7 @@ SHAPES = (BlockO, BlockI, BlockS, BlockZ, BlockL ,BlockJ, BlockT)
 
 class Tetris:
     """Implements a tetris game backend.
-    
+
     Interface
     ----------------------------------------------------------------------------
     attributes:
@@ -170,7 +170,7 @@ class Tetris:
         starting_row: int
         current_block: Block
         next_block: Block
-    
+
     methods:
         colored_squares
         game_step
@@ -182,16 +182,16 @@ class Tetris:
     def __init__(self, width, height, starting_row):
         assert height > 5 and width > 5
         self.grid = np.ones((width, height+1), dtype=np.int32)
-        
+
         # interface
         self.width = width
         self.height = height
         self.starting_row = starting_row
         self.current_block = None
         self.next_block = None
-        
+
         self.reset()
-    
+
     def colored_squares(self):
         # locked blocks
         for x in range(self.width):
@@ -199,20 +199,20 @@ class Tetris:
                 color = self.grid[x, y]
                 if color:
                     yield x, y, color
-        
+
         # moving block
         color = self.current_block.color
         for x, y in self.current_block.get_squares():
             yield x, y, color
-    
+
     def reset(self):
         self.grid[:, :-1] = 0
         self.current_block = self.get_random_block()
         self.next_block = self.get_random_block()
-    
+
     def __repr__(self):
         return self.current_block.grid_repr(self.grid)
-    
+
     def get_random_block(self):
         block_type = random.choice(SHAPES)
         block = block_type(
@@ -229,15 +229,15 @@ class Tetris:
         for _ in range(random.randrange(0, 4)):
             block.rotate(Rotation.direct)
         return block
-    
-    
+
+
     def erase_row(self, y):
         self.grid[:, y] = 0
-    
+
     def fall(self, y):
         for y2 in range(y, 0, -1):
             self.grid[:, y2] = self.grid[:, y2-1]
-    
+
     def lock_current_block(self):
         self.current_block.lock(self.grid)
         self.current_block = self.next_block
@@ -246,24 +246,24 @@ class Tetris:
             if np.all(self.grid[:, y]):
                 self.erase_row(y)
                 self.fall(y)
-    
+
     def is_over(self):
         return np.any(self.grid[:, :self.starting_row])
-    
+
     def game_step(self):
         self.current_block.shift(Direction.down)
         if self.current_block.colides(self.grid):
             self.current_block.shift(Direction.up)
             self.lock_current_block()
         return self.is_over()
-    
+
     def accelerate(self):
         while not self.current_block.colides(self.grid):
             self.current_block.shift(Direction.down)
         self.current_block.shift(Direction.up)
         self.lock_current_block()
         return self.is_over()
-    
+
     def _move_methods_fallback(do, undo):
         def move_current_block(self):
             do(self.current_block)
@@ -271,22 +271,22 @@ class Tetris:
                     or self.current_block.colides(self.grid)):
                 undo(self.current_block)
         return move_current_block
-    
+
     shift_left = _move_methods_fallback(
         lambda block: block.shift(Direction.left),
         lambda block: block.shift(Direction.right)
     )
-    
+
     shift_right = _move_methods_fallback(
         lambda block: block.shift(Direction.right),
         lambda block: block.shift(Direction.left)
     )
-    
+
     rotate_left = _move_methods_fallback(
         lambda block: block.rotate(Rotation.direct),
         lambda block: block.rotate(Rotation.indirect)
     )
-    
+
     rotate_right = _move_methods_fallback(
         lambda block: block.rotate(Rotation.indirect),
         lambda block: block.rotate(Rotation.direct)
--- tetris_front.py
+++ tetris_front.py
@@ -12,15 +12,15 @@ COLORS = [
 ]
 
 
-class KeyControlManager:    
+class KeyControlManager:
     def __init__(self, *, left_right_rotation_callbacks, left_right_shift_callback, down_callback):
         self.down_callback = down_callback
         self.shift_callbacks = left_right_shift_callback
         self.rotation_callbacks = left_right_rotation_callbacks
         self.last_rotation_callback = left_right_rotation_callbacks[0]
         self.rotate_mode = False
-    
-    
+
+
     def _input_methods_fallback(side):
         def input_method(self):
             if self.rotate_mode:
@@ -30,19 +30,19 @@ class KeyControlManager:
             else:
                 self.shift_callbacks[side]()
         return input_method
-    
+
     left = _input_methods_fallback(0)
     right = _input_methods_fallback(1)
-    
+
     def down(self):
         if self.rotate_mode:
             self.last_rotation_callback()
-        else:        
+        else:
             self.down_callback()
-        
+
     def enable_rotate_mode(self):
         self.rotate_mode = True
-    
+
     def disable_rotate_mode(self):
         self.rotate_mode = False
 
@@ -51,7 +51,7 @@ class TetrisButtons(tk.Frame):
     def __init__(self, master, font):
         super().__init__(master)
         self.font = font
-        
+
         self.button_shift_left = tk.Button(
             self,
             text='←',
@@ -98,13 +98,14 @@ class TetrisButtons(tk.Frame):
 class TetrisGui(tk.Tk):
     def __init__(self, back, factor=50, font_size=5, time=500, repeat=-1):
         super().__init__()
-        
+
         self.back = back
-        
+
         self.factor = factor
         self.time = time
         self.repeat = repeat
-        
+        self.scheduled_step = None
+
         self.color_grid = tk.Canvas(
             self,
             width=self.back.width * self.factor,
@@ -112,68 +113,83 @@ class TetrisGui(tk.Tk):
             bg='grey'
         )
 
-        
+
         self.buttons = TetrisButtons(self, Font(family='Helvetica', size=font_size))
         self.key_controls = KeyControlManager(
             left_right_rotation_callbacks=(self.rotate_left, self.rotate_right),
             left_right_shift_callback=(self.shift_left, self.shift_right),
             down_callback=self.accelerate
-        )        
+        )
         self.bind('<Left>', lambda _: self.key_controls.left())
         self.bind('<Right>', lambda _: self.key_controls.right())
         self.bind('<Down>', lambda _: self.key_controls.down())
         self.bind('<KeyPress-Control_L>', lambda _: self.key_controls.enable_rotate_mode())
         self.bind('<KeyRelease-Control_L>', lambda _: self.key_controls.disable_rotate_mode())
-        
+
         self.bind_all('<Escape>', lambda _: self.back.reset())
         self.bind_all('<KeyPress-q>', lambda _: self.quit())
-                
+
         self.color_grid.pack(side=tk.TOP)
         self.buttons.pack(side=tk.BOTTOM, expand=True)
-        
-        self.step()
-    
+
+        self.start_game()
+
     def reset(self):
         self.repeat -= 1
         if self.repeat != 0:
-            self.back.reset()
+            self.start_game()
         else:
             self.quit()
-    
+
+    def start_game(self):
+        if self.scheduled_step is not None:
+            self.after_cancel(self.scheduled_step)
+
+        self.back.reset()
+        self.color_grid.delete(tk.ALL)
+
+        y_limit = self.back.starting_row * self.factor
+        y_floor = self.back.height * self.factor
+        self.color_grid.create_line(0, y_limit, self.back.width * self.factor, y_limit)
+        for j in range(self.back.width):
+            x = j * self.factor
+            self.color_grid.create_line(x, y_limit, x, y_floor)
+
+        self.after(self.time, self.step)
+
     def step(self):
         if self.back.game_step():
             self.reset()
-        self.refresh()
+        self.update()
         self.after(self.time, self.step)
-    
-    def refresh(self):
-        self.color_grid.delete(tk.ALL)
-        y_limit = self.back.starting_row * self.factor
-        self.color_grid.create_line(0, y_limit, self.back.width * self.factor, y_limit)
+
+    def update(self):
+        self.color_grid.delete('block')
         for u, v, color_id in self.back.colored_squares():
             x, y = u*self.factor, v*self.factor
             self.color_grid.create_rectangle(
                 x, y, x+self.factor, y+self.factor,
-                fill=COLORS[color_id]
+                fill=COLORS[color_id],
+                tags='block'
             )
 
     def shift_left(self):
         self.back.shift_left()
-        self.refresh()
-    
+        self.update()
+
     def shift_right(self):
         self.back.shift_right()
-        self.refresh()
-    
+        self.update()
+
     def rotate_left(self):
         self.back.rotate_left()
-        self.refresh()
-    
+        self.update()
+
     def rotate_right(self):
         self.back.rotate_right()
-        self.refresh()
-    
+        self.update()
+
     def accelerate(self):
         if self.back.accelerate():
             self.reset()
-        self.refresh()
+        self.update()
